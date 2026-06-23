4701717625aad7310ce4a9a0b6109155e79ad383
Lug <shortsclips897@gmail.com>
2026-06-23 11:32:43 +0300

    redis

--- app/services/product_services.py
+++ app/services/product_services.py
@@ -33,7 +33,10 @@ async def get_products(session: AsyncSession, offset: int = 0, limit: int = 100)
 
 
 async def get_product_by_id(session: AsyncSession, id: int) -> Product:
+    if product := await get_cache(f"product:{id}"):
+        return product
     if product := await get_product(session=session, id=id):
+        await set_cache(f"product:{id}", ProductResponse.model_validate(product).model_dump())
         return product
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
 
@@ -50,6 +53,7 @@ async def update_product_by_id(session: AsyncSession, id: int, new_product_data:
     product = await get_product_by_id(session=session, id=id)
     product_udated = await update_product(session=session, product=product, new_product_data=new_product_data)
     await delete_cache("products:all")
+    await delete_cache(f"product:{id}")
     return product_udated
 
 
@@ -57,6 +61,7 @@ async def delete_product_by_id(session: AsyncSession, id: int) -> None:
     product = await get_product_by_id(session=session, id=id)
     await delete_product(session=session, product=product)
     await delete_cache("products:all")
+    await delete_cache(f"product:{id}")
 
 
 
